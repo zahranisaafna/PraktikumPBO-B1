3b7697e5b26aa13acf5cf90b612d9ed733384941
zahranisaafna <160565966+zahranisaafna@users.noreply.github.com>
2024-03-11 17:45:17 +0700

    Update MBankAccount.java

--- Tugas Prak ke 2/Tugas 2/MBankAccount.java
+++ Tugas Prak ke 2/Tugas 2/MBankAccount.java
@@ -8,8 +8,8 @@
     public static void main(String[] args) {
         BankAccount BA = new BankAccount(1000);
         System.out.println("Saldo sebelum transaksi: " + BA.getBalance());
-        System.out.println("Deposit sejumlah: "+ BA.deposit(500) + " berhasil.  Saldo saat ini: " + BA.getBalance());
-        System.out.println("Penarikan sejumlah: " + BA.withdraw(200) + " berhasil.  Saldo saat ini: " + BA.getBalance());
+        System.out.println("Deposit sejumlah "+ BA.deposit(500) + " berhasil.  Saldo saat ini: " + BA.getBalance());
+        System.out.println("Penarikan sejumlah " + BA.withdraw(200) + " berhasil.  Saldo saat ini: " + BA.getBalance());
         System.out.println("Saldo setelah transaksi: " + BA.getBalance());
     }
  
