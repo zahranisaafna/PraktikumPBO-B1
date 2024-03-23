9a96169548835b7f9659e3ac400045c0107ea177
zahranisaafna <160565966+zahranisaafna@users.noreply.github.com>
2024-03-23 15:33:30 +0700

    Add files via upload

---
+++ Praktikum 4/Tugas 4/bk/bukuFiksi/bukuNovel.java
@@ -0,0 +1,15 @@
+/*
+ * File : bukuNovel.java 03/21/2024
+ * Penulis :  Zahra Nisaa' Fitria Nur' Afifah
+ * NIM : 24060122140162
+ * Deskrispsi : representasi dasar dari objek buku Novel, turunan kelas bukuFiksi 
+ */
+package bk.bukuFiksi;
+
+
+public class bukuNovel extends bukuFiksi {
+    public  bukuNovel(String judul, String penulis, String tahunTerbit, String genre, int jumlahHalaman, double harga){
+        super(judul, penulis, tahunTerbit, genre, jumlahHalaman, harga);
+    }
+    
+}
---
+++ Praktikum 4/Tugas 4/bk/bukunonFiksi/bukuAkademik.java
@@ -0,0 +1,18 @@
+/*
+ * File : bukuAkademik.java 03/21/2024
+ * Penulis :  Zahra Nisaa' Fitria Nur' Afifah
+ * NIM : 24060122140162
+ * Deskrispsi : representasi dasar dari objek buku Akademik, turunan kelas bukuNonFiksi 
+ */
+package bk.bukunonFiksi;
+
+
+public class bukuAkademik extends bukuNonFiksi{
+    public bukuAkademik(String judul,String penulis,String tahunTerbit, String subjek, int jumlahHalaman, double harga){
+        super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
+    }
+    
+    
+    
+}
+
