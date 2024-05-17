3d7bd8c6a7f9fc7692c21c021782930b312f3ec7
zahranisaafna <160565966+zahranisaafna@users.noreply.github.com>
2024-05-17 08:26:24 +0700

    Delete Praktikum 8 directory

--- Praktikum 8/Tugas 8/11b.generic/Kupu.java
+++
@@ -1,9 +0,0 @@
-/*
- * File : Kupu.java 16/07/2024 
- * Deskripsi : kelas abstrak Kupu
- * Penulis :  Zahra Nisaa' Fitria Nur' Afifah
- * NIM : 24060122140162
- */
-public abstract class Kupu{
-    public abstract void gerak();
-}
